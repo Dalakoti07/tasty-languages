1575c2d8d0676ee695a7fe73ac6cce0588de67f6
dalakoti07 <saurabhdalakoti07@gmail.com>
2023-06-18 11:42:29 +0530

    added simple implementation

---
+++ go-lang/word-count/simple/wordCounterSimple.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+var outputFileName = "outputFile.txt"
+var fileName = "../sample.txt"
+
+func WriteResultsToFile(hashMap map[string]int) {
+	file, err := os.Create(outputFileName)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Write the hashmap to the file
+	for key, value := range hashMap {
+		line := fmt.Sprintf("%s: %v\n", key, value)
+		_, err := file.WriteString(line)
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+			return
+		}
+	}
+
+	fmt.Println("Hashmap written to output.txt")
+}
+
+func main() {
+	// calculate the time
+	startTime := time.Now()
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Panicf("error in reading file %v", err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic("Error in closing the file")
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	wordHashMap := make(map[string]int)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		eachWords := strings.Split(line, " ")
+		for _, word := range eachWords {
+			word = strings.ToLower(word)
+			wordHashMap[word]++
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Panicf("scanner got error: %v", err)
+	}
+
+	// end time
+	elapsedTime := time.Since(startTime).Milliseconds()
+	println("Time elapsed in milliseconds: ", elapsedTime)
+
+	WriteResultsToFile(wordHashMap)
+}
